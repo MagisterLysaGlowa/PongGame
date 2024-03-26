33d3758eaa169e9d1f130e7babe712a241b8e111
MagisterLysaGlowa <kacperpiaskowy937@gmail.com>
2024-03-26 12:09:08 +0100

    dodanie interfejsu ICanvasObject

--- PongGame/ICanvasObject.cs
+++ PongGame/ICanvasObject.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace PongGame
 {
     public interface ICanvasObject
     {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Canvas Canvas { get; set; }
+        public void Draw();
+        public void Reset();
     }
 }
