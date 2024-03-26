a7c7dbcdb367f030ddcf23e78d9261d0955e6ad3
MagisterLysaGlowa <kacperpiaskowy937@gmail.com>
2024-03-26 12:24:46 +0100

    dodanie podstawowych obiektów do canvas'u

--- PongGame/MainWindow.xaml.cs
+++ PongGame/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using PongGame.pong;
+using PongGame;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PongGame
 {
@@ -16,9 +19,23 @@ namespace PongGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer game_timer;
+        private Ball ball;
+        private Player player_mouse;
+        private Player player_keyboard;
+
         public MainWindow()
         {
             InitializeComponent();
+            //Utworzenie podstawowych obiektów
+
+            ball =
+                new Ball(10, 10, MainCanavs);
+            player_mouse =
+                new Player(MainCanavs, 10, 100, new SolidColorBrush(Colors.White), false);
+            player_keyboard =
+                new Player(MainCanavs, 10, 100, new SolidColorBrush(Colors.White), true);
+
         }
     }
 }
